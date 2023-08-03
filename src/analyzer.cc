ff77a2a76451a08cd4caa0b852eb53cd8b83045c
Chris G <chris@saturn.earth>
2023-08-02 20:46:27 -0500

    I have added the ability to write raw IQ data to stdout.  This allows me to pipe raw data to a different application (such as a demodulator).

--- src/analyzer.cc
+++ src/analyzer.cc
@@ -7,16 +7,22 @@
 // IQ (In-phase or Quadrature) signals that are provided by stdin.  The
 // data is 8-bit signed 2's complement, and is formatted as
 // I1,Q1; I2,Q2; ...
+// This program can also pass raw IQ data to stdout if required.
 //
 // To run this program type,
 // 
-//     ./analyzer > -d displaytype -f frequency -r sampleRate
-//                -d duration > ncoFileName,
+//     ./analyzer > -d displaytype -D
 //
 // where,
 //
-//    amplitude - 1 for magnitude display and 2 for power spectrum display.
-//    filename - Tne input source.
+//    displayType - The type of display.  Valid values are;
+//    1 - Magnitude display.
+//    2 - Power spectrum display.
+//
+//    The D flag indicates that raw IQ data should be dumped to stdout.
+//    This allows the data to be piped to another program.  Here's how
+//    to do this (for example, using a spectral display):
+//    ./analyzer -d 2 > >(other program to accept IQ data)
 ///*************************************************************************
 #include <stdio.h>
 #include <stdint.h>
@@ -31,6 +37,7 @@
 struct MyParameters
 {
   int *displayTypePtr;
+  bool *iqDumpPtr;
 };
 
 /*****************************************************************************
@@ -66,8 +73,11 @@ bool getUserArguments(int argc,char **argv,struct MyParameters parameters)
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
   // Default parameters.
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
-  // Default 1/2 scale.
+  // Default oscilloscope display.
   *parameters.displayTypePtr = SignalMagnitude;
+
+  // Default to not dumping IQ data.
+  *parameters.iqDumpPtr = false;
   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
 
   // Set up for loop entry.
@@ -79,7 +89,7 @@ bool getUserArguments(int argc,char **argv,struct MyParameters parameters)
   while (!done)
   {
     // Retrieve the next option.
-    opt = getopt(argc,argv,"d:h");
+    opt = getopt(argc,argv,"d:-Dh");
 
     switch (opt)
     {
@@ -89,10 +99,17 @@ bool getUserArguments(int argc,char **argv,struct MyParameters parameters)
         break;
       } // case
 
+      case 'D':
+      {
+        *parameters.iqDumpPtr = true;
+        break;
+      } // case
+
       case 'h':
       {
         // Display usage.
-        fprintf(stderr,"./analyzer -d [1 - magnitude | 2 - spectrum] \n");
+        fprintf(stderr,"./analyzer -d [1 - magnitude | 2 - spectrum] "
+                " -D (dump raw IQ)\n");
 
         // Indicate that program must be exited.
         exitProgram = true;
@@ -125,10 +142,12 @@ int main(int argc,char **argv)
   int8_t inputBuffer[16384];
   SignalAnalyzer *analyzerPtr;
   int displayType;
+  bool iqDump;
   struct MyParameters parameters;
 
   // Set up for parameter transmission.
   parameters.displayTypePtr = &displayType;
+  parameters.iqDumpPtr = &iqDump;
 
   // Retrieve the system parameters.
   exitProgram = getUserArguments(argc,argv,parameters);
@@ -172,6 +191,12 @@ int main(int argc,char **argv)
         } // case
 
       } // switch
+
+      if (iqDump == true)
+      {
+        // Write to stdout so that raw IQ can be piped to another program.
+        fwrite(inputBuffer,sizeof(int8_t),(2 * N),stdout);
+      } // if
 
     } // else
   } // while
